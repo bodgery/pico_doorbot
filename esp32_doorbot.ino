e2c030882099afa4cf57c70bc4a13ab4f6ad9851
tmurray <tmurray@wumpus-cave.net>
2021-02-26 18:43:59 -0600

    Initial commit

---
+++ esp32_doorbot.ino
@@ -0,0 +1,112 @@
+/*
+Copyright (c) 2020,  Timm Murray
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice, 
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, 
+      this list of conditions and the following disclaimer in the documentation 
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+
+const char* root_ca = \
+"-----BEGIN CERTIFICATE-----\n" \
+"MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\n" \
+"MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+"DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\n" \
+"PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\n" \
+"Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n" \
+"AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\n" \
+"rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\n" \
+"OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\n" \
+"xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n" \
+"7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\n" \
+"aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\n" \
+"HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\n" \
+"SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\n" \
+"ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\n" \
+"AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\n" \
+"R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\n" \
+"JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\n" \
+"Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n" \
+"-----END CERTIFICATE-----\n";
+
+const char* ssid = "The Bodgery";
+const char* psk = "0987654321";
+
+const int door_open_sec = 30;
+const int door_pin = 12;
+const int led_pin = 13;
+const char* location = "backdoor";
+
+const char* dump_keys_request = "https://rfid2.shop.thebodgery.org/secure/dump_active_tags";
+const char* check_key_request = "https://rfid2.shop.thebodgery.org/secure/entry/";
+
+
+void setup()
+{
+    Serial.begin( 115200 );
+
+    Serial.print( "Connecting to " );
+    Serial.print( ssid );
+    Serial.print( " " );
+    WiFi.begin( ssid, psk );
+    while( WiFi.status() != WL_CONNECTED ) {
+        delay( 1000 );
+        Serial.print( "." );
+    }
+    Serial.println( "Connected!" );
+
+    rebuild_cache();
+    // TODO Set a timer to rebuild the cache every 10 minutes
+}
+
+void loop()
+{
+    // TODO
+    // Read Wiegand
+    // Check if it's in the local cache
+    // If not, make a request to the server
+    // If either one passes, activate door for 30 seconds
+}
+
+
+void rebuild_cache()
+{
+    HTTPClient http;
+
+    Serial.println( "[CACHE] Rebuild cached keys" );
+    http.begin( dump_keys_request, root_ca );
+    http.addHeader( "Accept", "application/json" );
+    int status = http.GET();
+
+    if( 200 == status ) {
+        // TODO rebuild cache
+        Serial.println( "[CACHE] Fetched new key database" );
+    }
+    else {
+        Serial.print( "[CACHE] Error fetching new key database: " );
+        Serial.println( status );
+        Serial.println( http.getString() );
+    }
+
+    http.end();
+}
